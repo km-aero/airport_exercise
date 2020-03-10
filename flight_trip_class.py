c5f6c739d359c8ecfe0c512748a66b1106c1d127
Kevin Monteiro <kevin.monteiro96@gmail.com>
2020-03-10 17:15:19 +0000

    created main file

--- flight_trip_class.py
+++ flight_trip_class.py
@@ -1,6 +1,10 @@
 class FlightTrip:
-    def __init__(self, origin, destination, list_of_passenger, plane_number):
+    def __init__(self, origin='', destination='', list_of_passenger='', plane_number=''):
         self.origin = origin
         self.destination = destination
         self.list_of_passenger = list_of_passenger
         self.plane_number = plane_number
+
+# use getters and setters
+
+# as a
--- passenger_class.py
+++ passenger_class.py
@@ -4,3 +4,4 @@ class Passenger(People):
     def __init__(self, name, passport_number):
         super().__init__(name)
         self.passport_number = passport_number
+
